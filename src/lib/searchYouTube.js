7a0e4b892b8f648a67bd5ca18e9ac9791846f00b
Bruce Johnson <bruce@yogainternational.com>
2019-02-21 23:52:19 -0500

    started working on serachYouTube part of sprint

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,20 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  var optionsObj = {
+    q: options.query,
+    maxResults: options.max,
+    key: options.key,
+    part: 'snippet',
+    type: 'video',
+  };
+
+  if (!options.max) {
+    optionsObj.maxResults = 5;
+  }
+
+  $.get('https://www.googleapis.com/youtube/v3/search', function(data) {
+    callback(data);
+  });
 };
 
 window.searchYouTube = searchYouTube;
