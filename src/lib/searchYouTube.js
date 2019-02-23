eb6506dbff88cce1782bde6f6503e96be6a0a71e
Bruce Johnson <bruce@yogainternational.com>
2019-02-23 15:23:36 -0500

    Complete creating reusable API helper

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,20 +1,30 @@
 var searchYouTube = (options, callback) => {
   // TODO
   var optionsObj = {
-    q: options.query,
-    maxResults: options.max,
-    key: options.key,
-    part: 'snippet',
-    type: 'video',
+    url: 'https://www.googleapis.com/youtube/v3/search', // idea
+    data: {
+      q: options.query,
+      maxResults: options.max,
+      key: options.key,
+      part: 'snippet',
+      type: 'video',
+      videoEmbeddable: 'true',
+    },
+    success: function(data) {
+      console.log(data.items);
+      callback(data.items);
+    },
+    error: function(err) {
+      console.log(err + ': This is an error');
+    }
   };
 
   if (!options.max) {
     optionsObj.maxResults = 5;
   }
 
-  $.get('https://www.googleapis.com/youtube/v3/search', function(data) {
-    callback(data);
-  });
+  $.get(optionsObj);
+
 };
 
 window.searchYouTube = searchYouTube;
